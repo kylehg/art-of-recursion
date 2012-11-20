ffcbd09c6ae429cec4868a4f483a482175835730
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-11-20 01:19:10 -0500

    3

--- Hw10.hs
+++ Hw10.hs
@@ -5,7 +5,7 @@ fact :: Int -> Int
 fact 0 = 1
 fact n = n * fact (n-1)
 
-factTR :: Int -> Int
+factTR :: Int -> Int -> Int
 factTR r 0 = r
 factTR r n = factTR (n*r) (n-1)
 
@@ -20,4 +20,16 @@ fact' = factTR 1
 reverse :: [a] -> [a]
 reverse = aux [] where
   aux l (x:xs) = aux (x:l) xs
-  aux l []     = l
+  aux l _      = l
+
+-- | 3. Tail-recursive version of Fibbonacci
+fib' 0 = 0
+fib' 1 = 1
+fib' n = fib' (n-1) + fib' (n-2)
+
+fib :: Int -> Int
+fib = aux 0 1 where
+  aux a b 0 = a
+  aux a b 1 = b
+  aux a b 2 = a + b
+  aux a b c = aux b (a+b) (c-1)
